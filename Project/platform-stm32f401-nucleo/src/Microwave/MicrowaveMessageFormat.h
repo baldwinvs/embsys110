2c2bb70542831dfdcb93bf33f7eb879a8ead3e4c
baldwinvs <baldwinvs@outlook.com>
2020-06-06 15:39:29 -0700

    updated msg enums, updated state diagram

--- Project/platform-stm32f401-nucleo/src/Microwave/MicrowaveMessageFormat.h
+++ Project/platform-stm32f401-nucleo/src/Microwave/MicrowaveMessageFormat.h
@@ -7,66 +7,64 @@
 namespace MicrowaveMsgFormat {
 
 enum class Destination : uint32_t {
-    APP = 0x4D617070,
-    DEV = 0x4D646576
+    APP = 0x4D617070,   // DEV->APP
+    DEV = 0x4D646576    // APP->DEV
 };
 
 enum class Type : uint8_t {
-    STATE   = 0x10,
-    SIGNAL  = 0x20,
-    UPDATE  = 0x40,
+    STATE   = 0x40,
+    SIGNAL  = 0x50,
+    UPDATE  = 0x60,
 };
 
 enum class State : uint32_t {
-    NONE                        = 0x10000000,
-    DISPLAY_CLOCK               = 0x10000001,
-    CLOCK_SELECT_HOUR_TENS      = 0x10000002,
-    CLOCK_SELECT_HOUR_ONES      = 0x10000004,
-    CLOCK_SELECT_MINUTE_TENS    = 0x10000008,
-    CLOCK_SELECT_MINUTE_ONES    = 0x10000010,
-    SET_COOK_TIMER_INITIAL      = 0x10000020,
-    SET_COOK_TIMER_FINAL        = 0x10000040,
-    SET_POWER_LEVEL             = 0x10000080,
-    KITCHEN_SELECT_HOUR_TENS    = 0x10000100,
-    KITCHEN_SELECT_HOUR_ONES    = 0x10000200,
-    KITCHEN_SELECT_MINUTE_TENS  = 0x10000400,
-    KITCHEN_SELECT_MINUTE_ONES  = 0x10000800,
-    DISPLAY_TIMER_RUNNING       = 0x10000100,
-    DISPLAY_TIMER_PAUSED        = 0x10000200,
+    NONE                        = 0x404D3030,
+    DISPLAY_CLOCK,              // DEV->APP
+    CLOCK_SELECT_HOUR_TENS,     // DEV->APP
+    CLOCK_SELECT_HOUR_ONES,     // DEV->APP
+    CLOCK_SELECT_MINUTE_TENS,   // DEV->APP
+    CLOCK_SELECT_MINUTE_ONES,   // DEV->APP
+    SET_COOK_TIMER,             // DEV->APP
+    SET_POWER_LEVEL,            // DEV->APP
+    KITCHEN_SELECT_HOUR_TENS,   // DEV->APP
+    KITCHEN_SELECT_HOUR_ONES,   // DEV->APP
+    KITCHEN_SELECT_MINUTE_TENS, // DEV->APP
+    KITCHEN_SELECT_MINUTE_ONES, // DEV->APP
+    DISPLAY_TIMER,              // DEV->APP
 };
 
 enum class Signal : uint32_t {
-    NONE            = 0x20000000,
-    CLOCK           = 0x20000001,
-    COOK_TIME       = 0x20000002,
-    POWER_LEVEL     = 0x20000004,
-    KITCHEN_TIMER   = 0x20000008,
-    STOP            = 0x20000010,
-    START           = 0x20000020,
-    DIGIT_0         = 0x20000040,
-    DIGIT_1         = 0x20000080,
-    DIGIT_2         = 0x20000100,
-    DIGIT_3         = 0x20000200,
-    DIGIT_4         = 0x20000400,
-    DIGIT_5         = 0x20000800,
-    DIGIT_6         = 0x20001000,
-    DIGIT_7         = 0x20002000,
-    DIGIT_8         = 0x20004000,
-    DIGIT_9         = 0x20008000,
-    BLINK_ON        = 0x20010000,
-    BLINK_OFF       = 0x20020000,
-    MOD_LEFT_TENS   = 0x20040000,
-    MOD_LEFT_ONES   = 0x20080000,
-    MOD_RIGHT_TENS  = 0x20100000,
-    MOD_RIGHT_ONES  = 0x20200000,
-	STATE_REQUEST   = 0x20400000
+    NONE            = 0x504D3030,
+    CLOCK,          // APP<->DEV
+    COOK_TIME,      // APP<->DEV
+    POWER_LEVEL,    // APP<->DEV
+    KITCHEN_TIMER,  // APP<->DEV
+    STOP,           // APP->DEV
+    START,          // APP->DEV
+    DIGIT_0,        // APP->DEV
+    DIGIT_1,        // APP->DEV
+    DIGIT_2,        // APP->DEV
+    DIGIT_3,        // APP->DEV
+    DIGIT_4,        // APP->DEV
+    DIGIT_5,        // APP->DEV
+    DIGIT_6,        // APP->DEV
+    DIGIT_7,        // APP->DEV
+    DIGIT_8,        // APP->DEV
+    DIGIT_9,        // APP->DEV
+    BLINK_ON,       // DEV->APP
+    BLINK_OFF,      // DEV->APP
+    MOD_LEFT_TENS,  // DEV->APP
+    MOD_LEFT_ONES,  // DEV->APP
+    MOD_RIGHT_TENS, // DEV->APP
+    MOD_RIGHT_ONES, // DEV->APP
+    STATE_REQUEST   // APP->DEV
 };
 
 enum class Update : uint32_t {
-    NONE            = 0x40000000,
-    CLOCK           = 0x40000001,
-    DISPLAY_TIMER   = 0x40000002,
-    POWER_LEVEL     = 0x40000004,
+    NONE            = 0x604D3030,
+    CLOCK,          // DEV->APP
+    DISPLAY_TIMER,  // DEV->APP
+    POWER_LEVEL,    // DEV->APP
 };
 
 class Time {
@@ -90,10 +88,10 @@ public:
     }
     void clear()
     {
-    	left_tens = 0;
-    	left_ones = 0;
-    	right_tens = 0;
-    	right_ones = 0;
+        left_tens = 0;
+        left_ones = 0;
+        right_tens = 0;
+        right_ones = 0;
     }
     uint32_t left_tens;
     uint32_t left_ones;
@@ -110,7 +108,7 @@ public:
     {
         memcpy(this, rxData, sizeof(Message));
     }
-    
+
     ~Message() = default;
 
     Message(const Message&) = default;
